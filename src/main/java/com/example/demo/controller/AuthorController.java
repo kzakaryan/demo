6f5b97a46a11813f4c27eb25756c63dff2b32623
kzakaryan <kzakaryan@griddynamics.com>
2025-03-08 17:56:59 +0400

    Issue Resolved

--- src/main/java/com/example/demo/controller/AuthorController.java
+++ src/main/java/com/example/demo/controller/AuthorController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.repositories.AuthorRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthorController {
 
     private final AuthorRepository authorRepository;
--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,11 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.repositories.BookRepository;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BookController {
 
     private final BookRepository bookRepository;
